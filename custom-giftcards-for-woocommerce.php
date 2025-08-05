d3c93d506ca546d238d3ad3d1ae4fe505f2dbfc9
butuzoff <info@adev.com.ua>
2025-08-05 18:23:13 +0200

    chore: bump version to 1.0.15 для тестирования автоматического changelog

--- custom-giftcards-for-woocommerce.php
+++ custom-giftcards-for-woocommerce.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: Custom Giftcards for WooCommerce
  * Description: Gift card checkout, PDF download, balance tracking.
- * Version: 1.0.14
+ * Version: 1.0.15
  * Author: FLANCER.EU
  */
 
 defined( 'ABSPATH' ) || exit;
 define( 'CGFWC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'CGFWC_VERSION', '1.0.14' );
+define( 'CGFWC_VERSION', '1.0.15' );
 
 // Load email shipping method early to ensure proper initialization
 
